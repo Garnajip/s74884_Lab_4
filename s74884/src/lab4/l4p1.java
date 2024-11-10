497d19e5a3407264c7436820ea03ef3f57adc5aa
MP3_043 <MP3_043@172.17.77.81>
2024-11-10 09:31:43 +0800

    tiada

---
+++ s74884/src/lab4/l4p1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+/**
+ *
+ * @author MP3_043
+ */
+public class l4p1 {
+    
+}
